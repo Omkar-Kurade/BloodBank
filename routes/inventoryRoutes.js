d86289169aed35fa4e3ca40c4cf6feb9d7697bdb
Omkar Kurade <143413299+Omkar-Kurade@users.noreply.github.com>
2024-02-03 19:17:46 +0530

    final code
    BloodHub MERN Project

---
+++ routes/inventoryRoutes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const authMiddelware = require("../middlewares/authMiddelware");
+const {
+  createInventoryController,
+  getInventoryController,
+  getDonarsController,
+  getHospitalController,
+  getOrgnaisationController,
+  getOrgnaisationForHospitalController,
+  getInventoryHospitalController,
+  getRecentInventoryController,
+} = require("../controllers/inventoryController");
+
+const router = express.Router();
+
+//routes
+// ADD INVENTORY || POST
+router.post("/create-inventory", authMiddelware, createInventoryController);
+
+//GET ALL BLOOD RECORDS
+router.get("/get-inventory", authMiddelware, getInventoryController);
+
+//GET RECENT BLOOD RECORDS
+router.get(
+  "/get-recent-inventory",
+  authMiddelware,
+  getRecentInventoryController
+);
+
+//GET HOSPITAL BLOOD RECORDS
+router.post(
+  "/get-inventory-hospital",
+  authMiddelware,
+  getInventoryHospitalController
+);
+
+//GET DONAR RECORDS
+router.get("/get-donars", authMiddelware, getDonarsController);
+
+//GET Hospital RECORDS
+router.get("/get-hospitals", authMiddelware, getHospitalController);
+
+//GET Orgnaisation RECORDS
+router.get("/get-orgnaisation", authMiddelware, getOrgnaisationController);
+
+//GET orgnaisation for hospital RECORDS
+router.get(
+  "/get-orgnaisation-for-hospital",
+  authMiddelware,
+  getOrgnaisationForHospitalController
+);
+
+module.exports = router;
